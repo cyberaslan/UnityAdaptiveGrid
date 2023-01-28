7b1364d27cfca475032d3419ca9d4ad3d170803c
Kirill Korneev <gpflv@ya.ru>
2023-01-28 21:53:52 +0300

    Grid arrange layout, Fit scale method

--- Assets/Adaptive Grid/AdaptiveGrid.cs
+++ Assets/Adaptive Grid/AdaptiveGrid.cs
@@ -4,57 +4,58 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System;
 
+[ExecuteInEditMode]
 public class AdaptiveGrid : UIBehaviour
 {
-    private RectTransform _gridRect;
-    List<RectTransform> _gridChildren = new List<RectTransform>();
+    private event Action SettingsChanged;
+
+    [SerializeField] private RectTransform _gridRect;
+    [SerializeField] List<RectTransform> _gridChildren = new List<RectTransform>();
 
     private Vector2 _cellSize;
 
     /* Grid arrange and margins*/
-    [SerializeField] private Strategy _arrangeStrategy;
+    
     public enum ArrangeLayout { Fill = 0, Pack = 1, Grid = 2, FixedRows = 3, FixedColumns = 4 }
     [Header("Grid settings")]
     [SerializeField] private ArrangeLayout _arrangeLayout;
+    [SerializeField][SerializeReference] private Strategy _arrangeStrategy;
     [SerializeField] Offset _gridMargin;
     public void SetArrangeLayout(ArrangeLayout newArrangeLayout) {
-        _arrangeLayout = newArrangeLayout;
-        UpdateStrategies();
+        if (newArrangeLayout == _arrangeLayout) return; 
+        _arrangeLayout = newArrangeLayout; 
+        SettingsChanged?.Invoke();
     }
 
     /* Cell content scaling, padding and spacing */
-    [SerializeField] private Strategy _scaleStrategy;
     public enum ScaleMethod { Stretch = 0, Fit = 1, FitWidth = 2, FitHeight = 3 }
     [Header("Cell content")]
     [SerializeField] private ScaleMethod _scaleMethod;
+    [SerializeField] [SerializeReference] private Strategy _scaleStrategy;
     public void SetScaleMethod(ScaleMethod newScaleMethod) {
+        if (newScaleMethod == _scaleMethod) return;
         _scaleMethod = newScaleMethod;
-        UpdateStrategies();
+        SettingsChanged?.Invoke();
     }
     [SerializeField] Offset _cellPadding;
     [SerializeField] Offset _cellSpacing;
-    
 
 
     protected override void Awake() {
-        Activator.CreateInstance(typeof(FitScale));
         base.Awake();
         _gridRect = GetComponent<RectTransform>();
         CollectChildren();
-        ArrangeChildren();
-        
+        UpdateStrategies();
     }
-
-    void CollectChildren() {
+    protected override void Start() {
+        ArrangeChildren();
+    }
+    private void CollectChildren() {
         _gridChildren.Clear();
         foreach (RectTransform child in transform) _gridChildren.Add(child);
     }
 
-    void OnTransformChildrenChanged() {
-        CollectChildren();
-    }
-
-    void ArrangeChildren() {
+    private void ArrangeChildren() {
         _arrangeStrategy.Apply(_gridChildren, _gridRect);
         _scaleStrategy.Apply(_gridChildren, _gridRect);
     }
@@ -64,23 +65,40 @@ public class AdaptiveGrid : UIBehaviour
         StrategyMap.SetStrategy(ref _scaleStrategy, _scaleMethod);
     }
 
-#if UNITY_EDITOR
 
+    protected override void OnRectTransformDimensionsChange() {
+        Debug.Log("OnRectTransformDimensionsChange");
+        ArrangeChildren();
+    }
+
+    public void OnTransformChildrenChanged() {
+        Debug.Log("OnTransformChildrenChanged");
+        CollectChildren();
+        ArrangeChildren();
+    }
+
+#if UNITY_EDITOR
     protected override void OnValidate() {
         base.OnValidate();
         UpdateStrategies();
+        ArrangeChildren();
     }
 #endif
 
-    protected override void OnRectTransformDimensionsChange() { 
-        if(IsActive()) ArrangeChildren(); 
-    }
+
 }
 [Serializable]
-public class GridSize
+public struct GridSize  
 {
     public int Rows;
-    public int Columns;
+    public int Cols;
+    public GridSize(int rows, int columns) {
+        Rows = rows;
+        Cols = columns;
+    }
+    public override string ToString() {
+        return $"GridSize {Cols}x{Rows}";
+    }
 }
 [Serializable]
 public class Offset
--- Assets/Adaptive Grid/Strategy.cs
+++ Assets/Adaptive Grid/Strategy.cs
@@ -8,30 +8,34 @@ using System.Linq;
 public class StrategyMap
 {
     // Initializes dictionary with all types inherited from Strategy by relevant enum value keys in AdaptiveGrid settings
-    private static Dictionary<System.Enum, Strategy> _map { get; set; }
+    private static Dictionary<System.Enum, Strategy> _map { get; set;  }
     
     static StrategyMap() {
         _map = new Dictionary<System.Enum, Strategy>();
         IEnumerable<Type> inheritedStrategyTypes =
             Assembly.GetAssembly(typeof(Strategy)).GetTypes().Where(type => type.IsSubclassOf(typeof(Strategy)));
 
-        foreach (Type t in inheritedStrategyTypes) {
+        foreach (Type t in inheritedStrategyTypes) {  
             Strategy instance = (Strategy)Activator.CreateInstance(t);
             _map.Add(instance.SelectorInInspector, instance);
         }
     }
     public static void SetStrategy(ref Strategy strategy, System.Enum e) {
-        if (strategy != null) if (strategy?.SelectorInInspector == e) return;
-        Debug.Log($"New strategy: {e}");
+        if (strategy == null) {
+            strategy = GetStrategy(e);
+            return;     
+        }
+        if (strategy.SelectorInInspector.Equals(e)) return;
         strategy = GetStrategy(e);
     }
     public static Strategy GetStrategy(System.Enum e) {
-        return _map.FirstOrDefault(p => p.Value.SelectorInInspector.Equals(e)).Value;
+        return (Strategy) _map[e].Clone();
     }
 }
 
-public abstract class Strategy
+public abstract class Strategy : ICloneable 
 {
     public abstract System.Enum SelectorInInspector { get; }
     public abstract void Apply(List<RectTransform> elements, RectTransform grid);
+    public object Clone() { Debug.Log("Clone"); return this.MemberwiseClone(); }
 }
